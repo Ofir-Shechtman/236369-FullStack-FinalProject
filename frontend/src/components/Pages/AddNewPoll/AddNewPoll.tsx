117290d4c110af1bf92e85c5e613e6092beaaf1a
ofir511996 <ofir511996@gmail.com>
2022-01-08 16:45:11 +0200

    finish add poll form logic

--- frontend/src/components/Pages/AddNewPoll/AddNewPoll.tsx
+++ frontend/src/components/Pages/AddNewPoll/AddNewPoll.tsx
@@ -8,60 +8,30 @@ import '../../../App.css';
 import './AddnewPoll.css'
 
 import {FormValues} from "./FormValues";
-import {TextFieldForm} from './Forms'
+import {TextFieldForm, PollTypeForm, SwitchForm, MUITextField} from './Forms'
 
 
 
 export default function AddNewPoll() {
   const { register, handleSubmit, formState: { errors } , control} = useForm<FormValues>();
   const [multiple_enable, multiple_switch] = React.useState<Boolean>(true);
+  const [is_poll_type, poll_type_switch] = React.useState<string>("1");
   const onSubmit = (data: any) => alert(JSON.stringify(data));
+  const divOnChange = (e: { target: { value: any; }; }) => {
+        multiple_switch(!multiple_enable);
+        poll_type_switch(e.target.value);
+    };
   return (
     <div className="App">
       <h1>React Hook Form - Resolver</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
 
-        <TextFieldForm name={"Poll Name"} value={"PollName"} register={register} error={errors.PollName?.message}/>
-        <TextFieldForm name={"Poll Question"} value={"PollQuestion"} register={register} error={errors.PollQuestion?.message}/>
-        <section>
-        <label>Poll Type</label>
-        <Controller
-          render={({ field }) => (
-            <Select {...field}>
-              <MenuItem value="1">Telegram Poll</MenuItem>
-              <MenuItem value="2">Telegram Inline Keyboard</MenuItem>
-            </Select>
-          )}
-          name="PollType"
-          control={control}
-        />
-      </section>
-
-        <section>
-        <label>Allow Multiple Answers</label>
-        <Controller
-          name="MultipleAnswers"
-          control={control}
-          render={({ field }) => (
-            <Switch
-              onChange={(e) => field.onChange(e.target.checked)}
-              checked={!!field.value}
-              defaultChecked={false}
-              disabled={false}
-            />
-          )}
-        />
-      </section>
-        <section>
-        <label>MUI TextField</label>
-        <Controller
-          render={({ field }) => <TextField {...field} placeholder="Poll Name"/>}
-          name="PollName"
-          rules={{ required: true }}
-          control={control}
-        />
-      </section>
-
+        {/*<TextFieldForm name={"Poll Name"} value={"PollName"} register={register} error={errors.PollName?.message}/>*/}
+        {/*<TextFieldForm name={"Poll Question"} value={"PollQuestion"} register={register} error={errors.PollQuestion?.message}/>*/}
+        <MUITextField name={"Poll Name"} value={"PollName"} control={control} error={errors.PollName?.message}/>
+        <MUITextField name={"Poll Question"} value={"PollQuestion"} control={control} error={errors.PollQuestion?.message}/>
+        <PollTypeForm name={"Poll Type"} value={"PollType"} control={control} onChangeType={divOnChange} type_value={is_poll_type}/>
+        <SwitchForm name={"Allow Multiple Answers"} value={"MultipleAnswers"} control={control} multiple_enable={multiple_enable}/>
         <input type="submit" />
       </form>
     </div>
--- frontend/src/components/Pages/AddNewPoll/Forms.tsx
+++ frontend/src/components/Pages/AddNewPoll/Forms.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {PageLayoutProps} from "../PageLayout";
 import {FormValues} from "./FormValues";
-import {FieldPath} from 'react-hook-form';
+import {Controller, FieldPath} from 'react-hook-form';
 import {Switch} from "@material-ui/core";
 import {TextField, Select, MenuItem} from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
@@ -22,12 +22,95 @@ export const TextFieldForm: React.FC<TextFieldProps> = ({
                                                       }) => {
     return (
         <div>
-          <label>name</label>
+          <label>{name}</label>
           <input
-              {...register({value},
+              {...register(value,
                   {required: "Name is required",minLength: {value:5, message: "Min length is 5"}})} placeholder={name}
           />
           <p>{error}</p>
         </div>
     )
 }
+
+
+export interface ControlledProps {
+    name: string;
+    value: FieldPath<FormValues>;
+    control: any;
+    error?: string;
+    multiple_enable?: Boolean;
+    onChangeType?: any;
+    type_value?: string;
+}
+
+export const PollTypeForm: React.FC<ControlledProps> = ({
+                                                          name,
+                                                          value,
+                                                          control,
+                                                          onChangeType,
+                                                          type_value
+                                                      }) => {
+
+    return (
+        <section>
+        <label>{name}</label>
+        <Controller
+          render={({ field }) => (
+            <Select {...field} onChange= {onChangeType} value={type_value}>
+              <MenuItem value="1">Telegram Poll</MenuItem>
+              <MenuItem value="2">Telegram Inline Keyboard</MenuItem>
+            </Select>
+          )}
+          name={value}
+          control={control}
+        />
+      </section>
+    )
+}
+
+
+export const SwitchForm: React.FC<ControlledProps> = ({
+                                                          name,
+                                                          value,
+                                                          control,
+                                                          multiple_enable
+                                                      }) => {
+    return (
+        <section>
+        <label>{name}</label>
+        <Controller
+          name={value}
+          control={control}
+          render={({ field }) => (
+            <Switch
+              onChange={(e) => field.onChange(e.target.checked)}
+              checked={!!field.value}
+              defaultChecked={false}
+              disabled={!multiple_enable}
+            />
+          )}
+        />
+      </section>
+    )
+}
+
+export const MUITextField: React.FC<ControlledProps> = ({
+                                                          name,
+                                                          value,
+                                                          control,
+                                                          error
+                                                      }) => {
+    return (
+        <section>
+        <label>{name}</label>
+        <Controller
+          render={({ field }) => <TextField {...field} placeholder="Poll Name"/>}
+          name={value}
+          rules={{ required: true }}
+          control={control}
+        />
+      <p>{error}</p>
+      </section>
+    )
+}
+
