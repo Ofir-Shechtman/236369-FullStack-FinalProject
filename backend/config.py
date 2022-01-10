bb13995bda05f44309ce2b1360d49511a6305e06
ofir511996 <ofir511996@gmail.com>
2022-01-10 17:15:59 +0200

    fix models.py (again)

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 BOT_TOKEN = "5026874157:AAGLISmllwAvfOGq6lB5OqxRfOF_wufIJWA"
-DATABASE_URL_LOCAL = r"postgresql://postgres:123456@localhost/project"
-DATABASE_URL = r"postgresql://ygfwwmmx:XIkJFcKU_UpbPNqWhNXL4jjWwUrVPfl2@rosie.db.elephantsql.com/ygfwwmmx"
+DATABASE_URL = r"postgresql://postgres:123456@localhost/project"
+DATABASE_URL_REMOTE = r"postgresql://ygfwwmmx:XIkJFcKU_UpbPNqWhNXL4jjWwUrVPfl2@rosie.db.elephantsql.com/ygfwwmmx"
+# DATABASE_URL = r"postgresql://avkfzype:H5lYTuJGyBTwy4JCzLV1E8RxLcUzpzKS@rosie.db.elephantsql.com/avkfzype"
 URL = "http://127.0.0.1:5000/"
 SUPER_ADMIN = 'admin', '236369'
 SECRET_KEY = '7c8f3fd98e31e4f24020145ccf5bac3bdd22831d8b39ff70b45e24eb036807f9'
