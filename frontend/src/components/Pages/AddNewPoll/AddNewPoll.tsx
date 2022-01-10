6c8c21a68ce781a5e8eb059b04be940690e71c20
ofir511996 <ofir511996@gmail.com>
2022-01-10 12:51:17 +0200

    add to AddNewPoll all forms needed

--- frontend/src/components/Pages/AddNewPoll/AddNewPoll.tsx
+++ frontend/src/components/Pages/AddNewPoll/AddNewPoll.tsx
@@ -1,42 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import {Switch} from "@material-ui/core";
-import {TextField} from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 import '../../../App.css';
-import './AddnewPoll.css'
 
 import {FormValues} from "./FormValues";
-import {TextFieldForm, PollTypeForm, SwitchForm, MUITextField} from './Forms'
+import {TextFieldForm, PollTypeForm, SwitchForm, MUITextField, MultipleOptions, CloseTimePicker} from './Forms'
+import {v4 as uuidv4} from "uuid";
 
 
 
 export default function AddNewPoll() {
-  const { register, handleSubmit, formState: { errors } , control} = useForm<FormValues>();
+  const {handleSubmit, control} = useForm<FormValues>();
   const [multiple_enable, multiple_switch] = React.useState<Boolean>(true);
   const [is_poll_type, poll_type_switch] = React.useState<string>("1");
-  const onSubmit = (data: any) => alert(JSON.stringify(data));
   const divOnChange = (e: { target: { value: any; }; }) => {
+        if(e.target.value==="1"){multiple_switch(true);}
+        if(e.target.value==="2"){multiple_switch(false);}
         multiple_switch(!multiple_enable);
         poll_type_switch(e.target.value);
     };
-  return (
+    const [inputFields, setInputFields] = useState([
+    { id: uuidv4(), Option: '' }, { id: uuidv4(), Option: '' }
+  ]);
+    const onSubmit = (data: any) => {
+      data['MultipleOptions'] = inputFields.map((inputField: { id: React.Key | null | undefined; Option: unknown; }) => (inputField.Option))
+      alert(JSON.stringify(data))
+    }
+    return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
-
-        {/*<TextFieldForm name={"Poll Name"} value={"PollName"} register={register} error={errors.PollName?.message}/>*/}
-        {/*<TextFieldForm name={"Poll Question"} value={"PollQuestion"} register={register} error={errors.PollQuestion?.message}/>*/}
-        <MUITextField name={"Poll Name"} value={"PollName"} control={control} error={errors.PollName?.message}/>
-        <MUITextField name={"Poll Question"} value={"PollQuestion"} control={control} error={errors.PollQuestion?.message}/>
-        <PollTypeForm name={"Poll Type"} value={"PollType"} control={control} onChangeType={divOnChange} type_value={is_poll_type}/>
+<Stack spacing={3}>
+        <MUITextField name={"Poll Name"} value={"PollName"} control={control}/>
+        <MUITextField name={"Poll Question"} value={"PollQuestion"} control={control}/>
+        <PollTypeForm name={"Poll Type"} value={"PollType"} control={control} onChange={divOnChange} type_value={is_poll_type}/>
         <SwitchForm name={"Allow Multiple Answers"} value={"MultipleAnswers"} control={control} multiple_enable={multiple_enable}/>
-        <input type="submit" />
+        <CloseTimePicker name={"Auto Close Time"} value={"AutoCloseTime"} control={control} multiple_enable={multiple_enable}/>
+        <MultipleOptions name={"Multiple Options"} inputFields={inputFields} setInputFields={setInputFields} />
+        <Button variant="contained" component="label">
+            Submit
+              <input type="submit" hidden />
+        </Button>
+  </Stack>
       </form>
     </div>
   );
