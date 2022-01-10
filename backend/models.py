049eb5fc344c2f22e82ec4ac1c739ddc7d4ae685
ofir511996 <ofir511996@gmail.com>
2022-01-10 17:08:32 +0200

    fix models.py

--- backend/models.py
+++ backend/models.py
@@ -101,7 +101,7 @@ class Poll(db.Model):
             'close_date': self.close_date,
             'created_by': self.created_by,
             'poll_options': [option.content for option in self.poll_options],
-            'poll_answers': [{'user': receiver.user.first_name, 'answers': [answer.option.content for answer in receiver.poll_answers], 'time_answered':receiver.time_answered} for receiver in self.poll_receivers]
+            'poll_answers': [{'user': receiver.user.first_name, 'answers': [answer.option.content for answer in receiver.poll_answers], 'time_answered':min([answer.time_answered for answer in receiver.poll_answers])} for receiver in self.poll_receivers]
         }
 
 @dataclass
