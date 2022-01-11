833c932604f988dc7a75dd74ab0d0a7fe987d84e
ofir511996 <ofir511996@gmail.com>
2022-01-11 14:20:47 +0200

    add Dialog to AddNewAdmin.js

--- frontend/src/components/Pages/AddNewAdmin/AddNewAdmin.js
+++ frontend/src/components/Pages/AddNewAdmin/AddNewAdmin.js
@@ -2,7 +2,16 @@ import React, { useRef, useState } from "react";
 import '../../../App.css';
 import { get, isEmpty, set } from "lodash-es";
 import { FormBuilder } from "@jeremyling/react-material-ui-form-builder";
-import { Avatar, Button, IconButton, InputAdornment } from "@mui/material";
+import {
+  Avatar,
+  Button, Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  InputAdornment
+} from "@mui/material";
 import { LockOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
 import { red } from "@mui/material/colors";
 import axios from "axios";
@@ -25,7 +34,16 @@ export default function AddNewAdmin(props) {
   const { setAuthType } = props;
   const [form, setForm] = useState({});
   const [showPassword, setShowPassword] = useState();
+  const [openAlert, setOpenAlert] = React.useState(false);
 
+  const handleClickOpen = (event) => {
+    event.preventDefault();
+    setOpenAlert(true);
+  };
+
+  const handleClose = () => {
+    setOpenAlert(false);
+  };
   const refs = useRef({});
 
   const updateForm = (updates) => {
@@ -36,13 +54,13 @@ export default function AddNewAdmin(props) {
     setForm(copy);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async () => {
     const ok = await validate(refs, form);
     if (!ok) {
       return;
     }
-    axios.post('api/add_admin', form)
+    axios.post('api/add_admin', form);
+    handleClose();
   };
 
   const fields = [
@@ -122,7 +140,7 @@ export default function AddNewAdmin(props) {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div style={{ width: "60%" }}>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleClickOpen}>
           <FormBuilder
             fields={fields}
             form={form}
@@ -139,6 +157,25 @@ export default function AddNewAdmin(props) {
             Sign Up
           </Button>
         </form>
+        <Dialog
+        open={openAlert}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete Poll?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to create new admin?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+          <Button onClick={() => handleSubmit()} autoFocus>Yes</Button>
+        </DialogActions>
+      </Dialog>
       </div>
     </div>
   );
